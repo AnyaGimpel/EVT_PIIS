147b77af911aba58287339fff90010305c479e31
AnyaGimpel <annagimpel70@gmail.com>
2023-09-14 11:52:47 +0300

    lr2

---
+++ PEES/lr2/main.js
@@ -0,0 +1,39 @@
+const createTable = (movies) => {
+    let table = document.createElement('table');
+    table.setAttribute('border','1px')
+    
+    let row = table.insertRow(); 
+    let cell1 = row.insertCell(); 
+    let cell2 = row.insertCell();
+    cell1.innerHTML = "Название фильма"
+    cell2.innerHTML = "Оценка"
+
+    for (let elem in movies) {
+        let row = table.insertRow(); // добавим строку
+    
+        let cell1 = row.insertCell(); // добавим ячейки
+        let cell2 = row.insertCell();
+        
+        cell1.innerHTML = elem; // вставим текст
+        cell2.innerHTML = movies[elem];   
+    }
+    
+    // Отобразим таблицу на странице
+    document.body.appendChild(table);
+}
+
+let personalMovieDB = {
+    privat: true,
+    movies: {}
+};
+
+let i = 0
+do {
+    let movieName = prompt("Один из последних просмотренных фильмов?");
+    let score = prompt("На сколько оцените его?");
+    if (movieName != null && score != null && movieName != '' && score != '' && movieName.length < 51) {
+        personalMovieDB.movies[movieName] = score;
+        i++
+    }
+} while (i != 3);
+ if (personalMovieDB.privat == false) createTable(personalMovieDB.movies);
